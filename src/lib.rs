b8db6e64a45845847a2c8c162ac6f14dfbda2293
hanishkvc <hanishkvc@gmail.com>
2022-08-15 12:12:00 +0530

    Add the core Fuzz trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//!
+//! FuzzerK - A fuzzing library
+//!
+//! HanishKVC, 2022
+//!
+
+
+///
+/// The trait that needs to be implemented by the different fuzzers
+///
+trait Fuzz {
+    /// Generate the next fuzzed output and append to the passed buf
+    fn append_fuzzed(&mut self, step: usize, buf: &mut Vec<u8>);
+}
+
 
 #[cfg(test)]
 mod tests {
