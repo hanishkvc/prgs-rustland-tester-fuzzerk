22a1207e5c4b3de3babca99ecfc366b4ca64952b
hanishkvc <hanishkvc@gmail.com>
2022-08-19 10:39:45 +0530

    CfgFiles:WIP: Add logic to get a array of values wrt a key

--- src/cfgfiles.rs
+++ src/cfgfiles.rs
@@ -26,26 +26,43 @@ pub trait FromStringVec {
     fn get_value(sv: &mut VecDeque<String>, key: &str, spacesprefix: usize) -> String {
         let cursp = Self::get_spacesprefix(sv);
         if cursp != spacesprefix {
-            panic!("ERRR:FromStringVec:{}:Prefix whitespaces mismatch:{} != {}", Self::get_name(), spacesprefix, cursp);
+            panic!("ERRR:FromStringVec:{}-{}:Prefix whitespaces mismatch:{} != {}", Self::get_name(), key, spacesprefix, cursp);
         }
         let l = sv.pop_front();
         if l.is_none() {
-            panic!("ERRR:FromStringVec:{}:No data to process", Self::get_name())
+            panic!("ERRR:FromStringVec:{}-{}:No data to process", Self::get_name(), key)
         }
         let l = l.unwrap();
         let l = l.trim();
         let lt = l.split_once(':');
         if lt.is_none() {
-            panic!("ERRR:FromStringVec:{}:key-value delimiter ':' missing", Self::get_name())
+            panic!("ERRR:FromStringVec:{}-{}:key-value delimiter ':' missing", Self::get_name(), key)
         }
         let lt = lt.unwrap();
         if lt.0 != key {
-            panic!("ERRR:FromStringVec:{}:Expected key {}, got key {}", Self::get_name(), key, lt.0)
+            panic!("ERRR:FromStringVec:{}-{}:Expected key {}, got key {}", Self::get_name(), key, key, lt.0)
         }
         if lt.1.len() == 0 {
-            panic!("ERRR:FromStringVec:{}:No value given for {}", Self::get_name(), lt.0)
+            panic!("ERRR:FromStringVec:{}-{}:No value given for {}", Self::get_name(), key, lt.0)
         }
         return lt.1.to_string();
+    }
+
+    fn get_values(sv: &mut VecDeque<String>, key: &str, spacesprefix: usize) -> Vec<String> {
+        let sheadval = Self::get_value(sv, key, spacesprefix);
+        if sheadval.len() != 0 {
+            panic!("ERRR:FromStringVec:{}-{}:has non array value {} ???", Self::get_name(), key, sheadval);
+        }
+        let spacesprefix = Self::get_spacesprefix(sv);
+        while true {
+            let cursp = Self::get_spacesprefix(sv);
+            if spacesprefix != cursp {
+                panic!("ERRR:FromStringVec:{}-{}:Prefix whitespaces mismatch:{} != {}", Self::get_name(), key, spacesprefix, cursp);
+            }
+            let l = sv.pop_front();
+            let curline = l.unwrap();
+        }
+        Vec::new()
     }
 
     fn get_name() -> String;
