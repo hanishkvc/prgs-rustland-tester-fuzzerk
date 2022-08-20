2aa1d9c176c35b034812920c3fea271694fd8739
hanishkvc <hanishkvc@gmail.com>
2022-08-20 16:29:53 +0530

    GenPrintableAscii: print all or only printable based on exe name

--- misc/gen_printable_ascii.c
+++ misc/gen_printable_ascii.c
@@ -5,11 +5,23 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
     for (int i = 0; i < 256; i++) {
-        if isprint(i) {
+        bool bDoPrint = false;
+        if (strncmp("gen_printable_ascii", argv[0], 19) == 0) {
+            if isprint(i) {
+                bDoPrint=true;
+            }
+        } else {
+            bDoPrint=true;
+        }
+        if (bDoPrint) {
             printf("%c, %d, 0x%x\n", i, i, i);
         }
     }
 }
+
+// vim: set tabstop=4 softtabstop=4 shiftwidth=4 expandtab:
