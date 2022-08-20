5f615a8abbcd847f92ed3e86e8d7942377204fee
hanishkvc <hanishkvc@gmail.com>
2022-08-20 20:19:39 +0530

    Main: Print generated fuzzed binary raw value as well as string

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ fn main() {
     let mut rtm = rtm::RunTimeManager::new();
     cfgfiles::parse_file(cla[1].as_str(), &mut rtm);
     let fci = rtm.fcimmuts(cla[2].as_str()).unwrap();
-    println!("{:?}", fci.get(1));
-    println!("{:?}", fci.get(2));
+    let got1 = fci.get(1);
+    println!("Got1:\n\t{:?}\n\t{}", got1, String::from_utf8_lossy(&got1));
+    let got2 = fci.get(2);
+    println!("Got2:\n\t{:?}\n\t{}", got2, String::from_utf8_lossy(&got2));
 }
