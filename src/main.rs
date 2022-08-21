644851134abdf7a7497f52abb95f348ceb699a60
hanishkvc <hanishkvc@gmail.com>
2022-08-21 12:54:07 +0530

    Bring in my loggerk module/crate, Update main to use same

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,17 @@
 use std::env;
 use fuzzerk::cfgfiles;
 use fuzzerk::rtm;
+use loggerk::*;
 
 fn main() {
-    println!("MinimalFuzzerKUtil");
+    log_init();
+    log_o("MinimalFuzzerKUtil");
     let cla: Vec<String> = env::args().collect();
     let mut rtm = rtm::RunTimeManager::new();
     cfgfiles::parse_file(cla[1].as_str(), &mut rtm);
     let fci = rtm.fcimmuts(cla[2].as_str()).unwrap();
     let got1 = fci.get(1);
-    println!("Got1:\n\t{:?}\n\t{}", got1, String::from_utf8_lossy(&got1));
+    log_i(&format!("Got1:\n\t{:?}\n\t{}", got1, String::from_utf8_lossy(&got1)));
     let got2 = fci.get(2);
-    println!("Got2:\n\t{:?}\n\t{}", got2, String::from_utf8_lossy(&got2));
+    log_i(&format!("Got2:\n\t{:?}\n\t{}", got2, String::from_utf8_lossy(&got2)));
 }
