c6008f1a9e7d73252ad05e4899eee127d49efd91
hanishkvc <hanishkvc@gmail.com>
2022-08-23 09:54:01 +0530

    VM: Add new wrt its structs
    Also ignore comments in the loaded program

--- src/vm.rs
+++ src/vm.rs
@@ -10,12 +10,25 @@ use std::fs;
 
 use crate::iob::IOBridge;
 
+
 struct Context {
     strs: HashMap<String, String>,
     ints: HashMap<String, isize>,
     iobs: HashMap<String, IOBridge>,
     lbls: HashMap<String, usize>,
 }
+
+impl Context {
+    fn new() -> Context {
+        Context {
+            strs: HashMap::new(),
+            ints: HashMap::new(),
+            iobs: HashMap::new(),
+            lbls: HashMap::new(),
+        }
+    }
+}
+
 
 enum Op {
     None,
@@ -37,6 +50,13 @@ struct VM {
 
 impl VM {
 
+    fn new() -> VM {
+        VM {
+            ctxt: Rc::new(Context::new()),
+            ops: Vec::new(),
+        }
+    }
+
     fn compile_letstr(&mut self, sargs: &str) {
         self.ops.push(Op::None);
     }
@@ -46,10 +66,14 @@ impl VM {
     }
 
     pub fn compile(ops: Vec<String>) -> VM {
+        let vm = VM::new();
         for sop in ops {
-            
+            let sop = sop.trim();
+            if sop.starts_with("#") {
+                continue;
+            }
         }
-
+        vm
     }
 
     pub fn load_prg(prgfile: String) -> VM {
