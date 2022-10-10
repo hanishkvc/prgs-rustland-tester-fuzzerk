78762599c6bfe77ee6d57fbbd80585980ef37e0a
hanishkvc <hanishkvc@gmail.com>
2022-10-10 13:16:13 +0530

    DataUtils+VM:Quick and dirty checking of next_token

--- src/vm.rs
+++ src/vm.rs
@@ -907,6 +907,12 @@ impl VM {
             log_d(&format!("IN :{}\n", l));
             let nl = datautils::remove_extra_whitespaces(l);
             log_d(&format!("OUT:{}\n", nl));
+            let mut tl = nl.clone();
+            while tl.len() > 0 {
+                let (tok, tlnext) = datautils::next_token(&tl).unwrap();
+                tl = tlnext;
+                log_d(&format!("[{}]", tok));
+            }
             ops.push(nl.to_string());
         }
         self.compile(ops);
