0f7f4f529732668890224db6f026b85aa334aba1
hanishkvc <hanishkvc@gmail.com>
2022-10-15 00:46:30 +0530

    DataUtils:HexToBinBuf: Remember 2 chars consumed per loop

--- src/datautils.rs
+++ src/datautils.rs
@@ -17,7 +17,7 @@ use core::convert::From;
 ///
 pub fn vu8_from_hex(ins: &str) -> Result<Vec<u8>, String> {
     let mut vu8 = Vec::new();
-    for i in (0..ins.len()).step_by(2) {
+    for i in (0..ins.len()-1).step_by(2) {
         let cu8 = u8::from_str_radix(&ins[i..i+2], 16);
         if cu8.is_err() {
             return Err(format!("ERRR:DU:VU8FromHex:{}>>{}<<:{}", ins, &ins[i..i+2], cu8.unwrap_err()));
